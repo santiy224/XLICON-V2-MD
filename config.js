b9a81e14b097f997fc8e423683ece8da80059b9f
santiy224 <74521429+santiy224@users.noreply.github.com>
2024-08-28 09:57:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,15 +9,15 @@ import axios from 'axios'
 global.owner = [
   ['918360234087', 'WHITE', true], 
   ['233208179431', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
+  ['917408351365', 'DHEERAJ SINGH-(𝘼𝙝𝙢𝙢𝙞)', true], 
   ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
   ['233533763772', 'Abraham Dwamena2', true],
   ['233268374753', 'Abraham Dwamena3', true]
 ] 
 
-global.mods = ['923184070915', '94789481495', '923184474176', '233268374753', '233508488192'] 
-global.prems = ['923184070915', '233208179431', '94789481495', '918360234087', '233268374753']
-global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087', '233268374753']
+global.mods = ['917408351365', '94789481495', '923184474176', '233268374753', '233508488192'] 
+global.prems = ['917408351365', '233208179431', '94789481495', '918360234087', '233268374753']
+global.allowed = ['917408351365', '233208179431', '94789481495', '918360234087', '233268374753']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = global.keysZens[Math.floor(global.keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -51,10 +51,10 @@ global.APIKeys = {
 }
 
 // Sticker WM
-global.botname = 'XLICON-V2'
+global.botname = 'DHEERAJ SINGH V-1'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
-global.author = '@XLICON-V2' 
+global.packname = 'MADE BY DHEERAJ ' 
+global.author = '@TDDHEERAJOP-V1' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
 global.igfg = '▢✓ Follow Our channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
 global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
